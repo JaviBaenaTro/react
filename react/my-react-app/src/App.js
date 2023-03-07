e6376a3f58e9df929a74982b280326d30666f239
JaviBaenaTro <javibaenatro15@alumnos.cei.es>
2023-03-07 12:37:21 +0100

    actual2

--- react/my-react-app/src/App.js
+++ react/my-react-app/src/App.js
@@ -1,14 +1,32 @@
 
 import { useState, useEffect, useRef, useReducer} from "react";
 
-const state = [{
+const state = [
+  {
   id:1,
   name: "terminar esta clase",
   isCompleted: false,
 },
 ];
 
-const reducer = () =>{}
+const reducer = (estado, accion) =>{
+  if(accion.type == "ADD_TODO"){
+    let name = accion.payload;
+    /* payload es como un comodin, a un no se sabe lo que se va a poner */
+    return [...estado,{id : Math.random(Math.floor())*9,name, isCompleted:false}]
+  }
+  if (condition.type == "DONE"){
+    let id = accion.payload;
+    let newStake = estado.map((unTodo) =>{
+      if (unTodo.id== id){
+        return {...unTodo, isCompleted :  !unTodo.isCompleted}
+      }
+      return unTodo;
+    });
+    return newState;
+
+  }
+}
 
 function App(){
 
