b5227695115e176c7f6ae92195d002040a387a43
JaviBaenaTro <javibaenatro15@alumnos.cei.es>
2023-03-07 12:04:14 +0100

    hola desde clase

--- react/my-react-app/src/App.js
+++ react/my-react-app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import { useEffect, useState, useRef } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let inputEl = useRef(null);
+
+  useEffect(() => {
+    inputEl.current.focus();
+  }, []),
+  };
+
+    return(<div>
+        <input ref={inputEl} type="text"> 
+        </input>
+        <button onClick={onClickBoton}>
+            enfoca!
+        </button>
+    </div>)
 }
+
+
 
 export default App;
